fd49c16c4585629ba81a29f596cc8d3dbbc2ba35
carlos-alex <cealex@gmail.com>
2020-04-08 17:04:55 +0100

    100 dont repeat yourself, dry

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController  < ApplicationController
 
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+	#before_action :article_params, only: [:create, :update]
+
 	def show
-		@article= Article.find(params[:id])
+		set_article
 	end
 
 	def index
@@ -13,11 +17,11 @@ class ArticlesController  < ApplicationController
   end
 
 	def edit
-		@article= Article.find(params[:id])
+		set_article
 	end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title,:description))
+		@article = Article.new(article_params)
 		if @article.save
 				flash[:notice]= "artigo bem criado."
 				redirect_to @article
@@ -27,8 +31,8 @@ class ArticlesController  < ApplicationController
 	end
 
 	def update
-		@article= Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:title,:description))
+		set_article
+		if @article.update(article_params)
 			flash[:notice]= "artigo update."
 			redirect_to @article
 		else
@@ -50,12 +54,19 @@ class ArticlesController  < ApplicationController
 #
 
 	def destroy
-		@article= Article.find(params[:id])
+		set_article
 		@article.destroy
 		redirect_to articles_path
 	end
 
+	private
 
+ 	def set_article
+		@article= Article.find(params[:id])
+	end
 
+	def article_params
+		params.require(:article).permit(:title,:description)
+	end
 
 end
